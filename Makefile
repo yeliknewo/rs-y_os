499fd5775586f6d87b68cda89b904c191e460141
= <yelikdev@gmail.com>
2015-12-17 21:35:55 -0500

    setting up long mode

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 arch ?= x86_64
-target ?= $(arch)-unkown-linux-gnu
+target := $(arch)-unknown-linux-gnu
 kernel := build/kernel-$(arch).bin
 
 os := build/os-$(arch).bin
 
 boot := src/assembly/$(arch)/boot/boot.asm
-rust_y_core := target/$(target)/debug/liby_core.a
 linker_script := src/other/linker.ld
+
 assembly_sources := $(wildcard src/assembly/$(arch)/*.asm)
 assembly_objects := $(patsubst src/assembly/$(arch)/%.asm, build/assembly/$(arch)/%.o, $(assembly_sources))
 
-.PHONY: all clean qemu
+rust_sources := $(wildcard src/rust/*.rs)
+rust_objects := $(patsubst src/rust/%.rs, build/rust/%.o, $(rust_sources))
+
+.PHONY: all clean qemu no_kernel cargocargo
 
 all: $(os)
 
@@ -21,11 +24,16 @@ qemu: all
 	@qemu-system-$(arch) -hda $(os) -boot c
 
 $(os): $(boot) $(kernel)
+	@mkdir -p $(shell dirname $@)
 	@nasm -f bin -o $@ $<
 
-$(kernel): $(assembly_objects)
+$(kernel): $(assembly_objects) $(rust_objects)
 	@ld -n -T $(linker_script) -o $(kernel) $<
 
 build/assembly/$(arch)/%.o: src/assembly/$(arch)/%.asm
 	@mkdir -p $(shell dirname $@)
 	@nasm -f elf64 -o $@ $<
+
+build/rust/%.o: src/rust/%.rs
+	@mkdir -p $(shell dirname $@)
+	@rustc --crate-type lib --target=$(target) -o $@ --emit obj $<
--- src/assembly/x86_64/boot/boot.asm
+++ src/assembly/x86_64/boot/boot.asm
@@ -5,7 +5,7 @@ real_mode:
     xor ax, ax      ; set ax to 0
     mov ds, ax      ; set ds to 0
     mov ss, ax      ; set ss to 0
-    mov sp, 0x9c00  ; make stack at end of this program
+    mov sp, stack_bottom  ; make stack at end of this program
 
     cli
     lgdt [gdtr]
@@ -16,8 +16,64 @@ real_mode:
 
     jmp 0x08:protected_mode
 
+
+bits 32
+protected_mode:
+    mov eax, 0x10
+    mov ds, eax
+    mov es, eax
+    mov fs, eax
+    mov gs, eax
+    mov ss, eax
+
+    mov esp, stack_bottom
+
+    call check_cpuid
+    call check_long_mode
+
+    mov dword [0xb8000], 0x2f4b2f4f
+
 hang:
     jmp hang
+
+error:
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte  [0xb800a], al
+    hlt
+
+check_cpuid:
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    mov ecx, eax         ; Set the C-register to the A-register.
+    xor eax, 1 << 21     ; Flip the ID-bit, which is bit 21.
+    push eax             ; Store the A-register.
+    popfd                ; Restore the FLAGS-register.
+    pushfd               ; Store the FLAGS-register.
+    pop eax              ; Restore the A-register.
+    push ecx             ; Store the C-register.
+    popfd                ; Restore the FLAGS-register.
+    xor eax, ecx         ; Do a XOR-operation on the A-register and the C-register.
+    jz .no_cpuid         ; The zero flag is set, no CPUID.
+    ret                  ; CPUID is available for use.
+.no_cpuid:
+    mov al, "0"
+    jmp error
+
+check_long_mode:
+    mov eax, 0x80000000    ; Set the A-register to 0x80000000.
+    cpuid                  ; CPU identification.
+    cmp eax, 0x80000001    ; Compare the A-register with 0x80000001.
+    jb .no_long_mode       ; It is less, there is no long mode.
+    mov eax, 0x80000001    ; Set the A-register to 0x80000001.
+    cpuid                  ; CPU identification.
+    test edx, 1 << 29      ; Test if the LM-bit is set in the D-register.
+    jz .no_long_mode       ; They aren't, there is no long mode.
+    ret
+.no_long_mode:
+    mov al, "1"
+    jmp error
 
 gdtr:
     dw (gdt_end - gdt) + 1  ; size
@@ -44,3 +100,7 @@ gdt_end:
 
 times 510-($-$$) db 0
 dw 0xAA55
+
+stack_bottom:
+    resb 64
+stack_top:
